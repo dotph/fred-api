ee0f4c0a486fd5913e7021287f8cfb23f6c60532
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-29 15:13:05 +0800

    Also generate JUNit test reports for Jenkins

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,12 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 
+require 'minitest/reporters'
+
+Minitest::Reporters.use! [
+  Minitest::Reporters::DefaultReporter.new,
+  Minitest::Reporters::JUnitReporter.new
+]
+
 class ActiveSupport::TestCase
 end
