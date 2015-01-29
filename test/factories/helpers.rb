33dadcd7a4014fff1c17a696ac6c0f12c7d9a229
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-30 00:23:48 +0800

    Include all properties for ContactCreate

--- test/factories/helpers.rb
+++ test/factories/helpers.rb
@@ -1,8 +1,11 @@
 def create_contact handle: 'contact_handle'
   request = create :contact_create_request
 
-  create_property request: request, name: 'handle',   value: 'contact_handle'
-  create_property request: request, name: 'pi.name',  value: 'Handle'
+  create_property request: request, name: 'handle',   value: handle
+
+  ['pi.name', 'pi.street', 'pi.city', 'pi.postalCode', 'pi.countryCode', 'email'].each do |name|
+    create_property request: request, name: name,  value: 'value'
+  end
 end
 
 def create_property name:, value:, request:
