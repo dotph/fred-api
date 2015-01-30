37a94954fe488e9b8f662ef0ec244e0743584436
Mike Mallete <mike@dot.ph>
2015-01-30 14:41:49 +0800

    Added job concerns

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module FREDAPI
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += %W(#{config.root}/app/jobs/concerns)
   end
 end
