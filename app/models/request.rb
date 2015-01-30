6ed308bdc66fba66a60098e4a76a90220dab3b0c
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-30 17:27:28 +0800

    Request.latest_time

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,10 @@ class Request < ActiveRecord::Base
   belongs_to  :request_type
 
   has_many :request_property_values
+
+  def self.latest_time current_time: Time.now
+    latest_record = order(time_end: :desc).first
+
+    latest_record ? latest_record.time_end : current_time
+  end
 end
--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -17,4 +17,20 @@ describe Request do
       @request.request_property_values.wont_be_empty
     end
   end
+
+  describe :latest_time do
+    it 'returns latest audit_time' do
+      create_contact on: '2015-01-30 5:26 PM'.to_time
+      create_contact on: '2015-01-30 5:27 PM'.to_time
+      create_contact on: '2015-01-30 5:28 PM'.to_time
+
+      Request.latest_time.must_equal '2015-01-30 5:28 PM'.to_time
+    end
+
+    it 'returns current time if no existing records' do
+      current_time = Time.now
+
+      Request.latest_time(current_time: current_time).must_equal current_time
+    end
+  end
 end
