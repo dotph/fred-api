89cbd7c36ae4f4e96191af5e0d0acdab7bcf930f
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-30 17:21:46 +0800

    SyncLog.last_run

--- app/models/sync_log.rb
+++ app/models/sync_log.rb
@@ -1,2 +1,7 @@
 class SyncLog < ActiveRecord::Base
+  def self.last_run current_time: Time.now
+    latest_record = order(until: :desc).first
+
+    latest_record ? latest_record.until.to_time : current_time
+  end
 end
--- test/models/sync_log_test.rb
+++ test/models/sync_log_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
 describe SyncLog do
+  describe :last_run do
+    it 'returns latest timestamp' do
+      SyncLog.create since: '2015-01-06 2:57 PM'.to_time, until: '2015-01-06 2:58 PM'.to_time
+      SyncLog.create since: '2015-01-06 2:58 PM'.to_time, until: '2015-01-06 2:59 PM'.to_time
+      SyncLog.create since: '2015-01-06 2:59 PM'.to_time, until: '2015-01-06 3:00 PM'.to_time
+
+      SyncLog.last_run.must_equal '2015-01-06 3:00 PM'.to_time
+    end
+
+    it 'returns current time if no existing records' do
+      current_time = Time.now
+
+      SyncLog.last_run(current_time: current_time).must_equal current_time
+    end
+  end
 end
