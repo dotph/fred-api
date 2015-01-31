bd7b84c52b9a00dbfbf3c06935ad016c8719cdbe
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-31 16:56:18 +0800

    RequestType::DOMAIN_CREATE

--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -1,5 +1,6 @@
 class RequestType < ActiveRecord::Base
-  CONTACT_CREATE = 'ContactCreate'
+  CONTACT_CREATE  = 'ContactCreate'
+  DOMAIN_CREATE   = 'DomainCreate'
 
   self.table_name = 'request_type'
 end
