dfa8bc134921975aec9cd28f65cb0919c5f0d774
Ealden Esto E. Escañan <ealden@dot.ph>
2015-01-31 16:24:07 +0800

    register_domain helper

--- test/factories/helpers.rb
+++ test/factories/helpers.rb
@@ -8,7 +8,20 @@ def create_contact handle: 'contact_handle', on: Time.now
   ['pi.name', 'pi.street', 'pi.city', 'pi.postalCode', 'pi.countryCode', 'email'].each do |name|
     create_property request: request, name: name,  value: 'value'
   end
+end
 
+def register_domain name: 'test.ph', on: Time.now
+  request = create :domain_create_request, time_begin: on, time_end: on
+
+  create_property request: request, name: 'handle',     value: name
+  create_property request: request, name: 'registrant', value: 'contact_handle'
+  create_property request: request, name: 'period',     value: 1
+  create_property request: request, name: 'timeunit',   value: 'Year'
+  create_property request: request, name: 'nsset',      value: 'ABC123'
+  create_property request: request, name: 'authInfo',   value: 'ABC123'
+
+  create_property request: request, name: 'rc',         value: 1000
+  create_property request: request, name: 'msg',        value: 'Command completed successfully'
 end
 
 def create_property name:, value:, request:
