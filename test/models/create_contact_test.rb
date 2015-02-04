28fdf1b8f726bb00ae717e198e166482c6f23340
Ealden Esto E. Escañan <ealden@dot.ph>
2015-02-04 12:26:48 +0800

    Remove first and instead put the actual result as subject

--- test/models/create_contact_test.rb
+++ test/models/create_contact_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe CreateContact do
   describe :all do
-    subject { CreateContact.all since: since, up_to: up_to }
+    subject { CreateContact.all(since: since, up_to: up_to).first }
 
     before do
       create_contact on: current_time + 1.minute
@@ -14,26 +14,26 @@ describe CreateContact do
       let(:since) { current_time }
       let(:up_to) { current_time + 1.minute }
 
-      specify { subject.count.must_equal 1 }
-      specify { subject.first.partner.must_equal 'alpha' }
-      specify { subject.first.partner.must_equal 'alpha' }
-      specify { subject.first.handle.must_equal 'contact_handle' }
-      specify { subject.first.name.must_equal 'value' }
-      specify { subject.first.organization.must_be_nil }
-      specify { subject.first.street.must_equal 'value' }
-      specify { subject.first.city.must_equal 'value' }
-      specify { subject.first.state.must_be_nil }
-      specify { subject.first.postal_code.must_equal 'value' }
-      specify { subject.first.country_code.must_equal 'value' }
-      specify { subject.first.phone.must_be_nil }
-      specify { subject.first.email.must_equal 'value' }
+      specify { subject.wont_be_nil }
+      specify { subject.partner.must_equal 'alpha' }
+      specify { subject.partner.must_equal 'alpha' }
+      specify { subject.handle.must_equal 'contact_handle' }
+      specify { subject.name.must_equal 'value' }
+      specify { subject.organization.must_be_nil }
+      specify { subject.street.must_equal 'value' }
+      specify { subject.city.must_equal 'value' }
+      specify { subject.state.must_be_nil }
+      specify { subject.postal_code.must_equal 'value' }
+      specify { subject.country_code.must_equal 'value' }
+      specify { subject.phone.must_be_nil }
+      specify { subject.email.must_equal 'value' }
     end
 
     context :no_contacts_match do
       let(:since) { Time.now }
       let(:up_to) { Time.now }
 
-      specify { subject.must_be_empty }
+      specify { subject.must_be_nil }
     end
   end
 
